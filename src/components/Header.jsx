95c3de66f29e82586349e8b46ce0cc017b4bfc82
jjeg1979 <jjeg1979@gmail.com>
2024-04-04 14:51:02 +0200

    AppLayout, Home, Header y Footer tras creación proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+      <header className="py-2 px-3 sm:py-4 sm:px-8 flex justify-between items-center bg-white border-b shadow-xs">
+        {/* Logo para version mobile */}
+        <img src="../blockmaker-logo.png"
+        alt="blockmaker-logo" width={47} className="sm:hidden" />
+        {/* Logo para version desktop */}
+        <img src="../blockmaker-full-logo.png" alt="blockmaker-logo" width={300} className="hidden sm:flex" />
+      </header>
+    )
+  }
