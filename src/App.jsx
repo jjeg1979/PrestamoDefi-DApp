9ca1893676656a3c02bae68be16ce17cdc97d580
jjeg1979 <jjeg1979@gmail.com>
2024-04-04 16:02:06 +0200

    Configuración conexión con el Wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { AppLayout } from './components/ui/layouts'
 import Home from './pages/Home'
+import { WagmiConfig } from 'wagmi'
+import { ConnectKitProvider } from 'connectkit'
+import { config } from './config/wagmi'
 
 function App() {
   return (
-    <>
-      <AppLayout>
-        <Home />
-      </AppLayout>
-    </>    
+    <WagmiConfig config={config}>
+      {/* Lo ponemos en modo light ya que construiremos la aplicación en modo claro */}
+      <ConnectKitProvider mode="light">
+        <AppLayout>
+          <Home />
+        </AppLayout>
+      </ConnectKitProvider>
+    </WagmiConfig>    
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { ConnectKitButton } from 'connectkit'
+
 export default function Header() {
     return (
       <header className="py-2 px-3 sm:py-4 sm:px-8 flex justify-between items-center bg-white border-b shadow-xs">
@@ -6,9 +8,9 @@ export default function Header() {
         alt="blockmaker-mobile-logo" width={47} className="sm:hidden" />
         {/* Logo para version desktop */}
         <img src="../blockmaker-full-logo.png" alt="blockmaker-desktop-logo" width={300} className="hidden sm:flex" />
-        <button>
-          Connect Wallet
-        </button>
+        {/*<button className="bg-gray-100 rounded-xl px-2 text-sm h-fit py-2">Connect Wallet </button> */}
+        {/* Botton de conexion wallet con prop showBalance para mostrar balance cuenta */}
+      <ConnectKitButton showBalance />
       </header>
     )
   }
