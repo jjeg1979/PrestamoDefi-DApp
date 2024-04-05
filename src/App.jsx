c55934b46ea63b596b7aad41e42d1c5e0fffd61d
jjeg1979 <jjeg1979@gmail.com>
2024-04-05 12:25:36 +0200

    Añadir componente Toast a la App

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Home from './pages/Home'
 import { WagmiConfig } from 'wagmi'
 import { ConnectKitProvider } from 'connectkit'
 import { config } from './config/wagmi'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
     <WagmiConfig config={config}>
+      <Toaster position="bottom-right"/>
       {/* Lo ponemos en modo light ya que construiremos la aplicación en modo claro */}
       <ConnectKitProvider mode="light">
         <AppLayout>
