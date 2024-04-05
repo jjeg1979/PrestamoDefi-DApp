f3fbddae8e95c153d5f7dceb0071e894b86085c6
jjeg1979 <jjeg1979@gmail.com>
2024-04-06 00:38:54 +0200

    Añadir y definir rutas aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { WagmiConfig } from 'wagmi'
 import { ConnectKitProvider } from 'connectkit'
 import { config } from './config/wagmi'
 import { Toaster } from 'react-hot-toast'
+import { Routes, Route } from 'react-router-dom'
+import { GestionUsuarios } from './pages/usuarios'
+import { GestionPrestamos } from './pages/prestamos'
 
 function App() {
   return (
@@ -12,6 +15,11 @@ function App() {
       {/* Lo ponemos en modo light ya que construiremos la aplicación en modo claro */}
       <ConnectKitProvider mode="light">
         <AppLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/usuarios" element={<GestionUsuarios />} />
+            <Route path="/prestamos" element={<GestionPrestamos />} />
+          </Routes>
           <Home />
         </AppLayout>
       </ConnectKitProvider>
