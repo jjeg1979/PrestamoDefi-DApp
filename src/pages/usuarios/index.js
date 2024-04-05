4d35206ccbf8d0cfba34e2180f13f32e241eec59
jjeg1979 <jjeg1979@gmail.com>
2024-04-06 00:40:18 +0200

    Añadir GestionUsuarios a index.js

--- src/pages/usuarios/index.js
+++ src/pages/usuarios/index.js
@@ -1,2 +1,3 @@
 export { default as AltaPrestamista } from "./AltaPrestamista";
 export { default as AltaCliente } from "./AltaCliente";
+export { default as GestionUsuarios } from "./GestionUsuarios";
