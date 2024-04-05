8e08d3778f31a7c7786ed0507c3fa35a6e65d119
jjeg1979 <jjeg1979@gmail.com>
2024-04-05 14:42:39 +0200

    Añadido botón conectar cuando no se está conectado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,18 @@ import { useAccount } from 'wagmi'
 import { LoadingSpinner, Button, ErrorInfo, TextInput, Title, SocioPrincipal } from '../components/ui'
 import AltaPrestamista from './usuarios/AltaPrestamista'
 import { AltaCliente } from './usuarios'
+import { ConnectKitButton } from 'connectkit'
 
 export default function Home() {
 
     const { address, isConnecting, isDisconnected } = useAccount()
 
     if (isConnecting) return <div>Connecting...</div>
-    if (isDisconnected) return <div>Disconnected</div>
+    if (isDisconnected) return (
+      <div>
+        <ConnectKitButton />
+      </div>
+      )
     return (
       <div className="flex flex-col justify-between gap-6">
         <div>Connected Wallet: {address}</div>
