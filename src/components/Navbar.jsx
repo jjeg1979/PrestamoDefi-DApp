365ab39d08fdeb874c82d88684af2a115e5de6fb
jjeg1979 <jjeg1979@gmail.com>
2024-04-06 00:22:53 +0200

    Crear componente Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <nav className="flex gap-5 text-xl">
+          <Link to="/" className="text-gray-500 hover:text-gray-900">Inicio</Link>
+          <Link to="/usuarios" className="text-gray-500 hover:text-gray-900">Usuarios</Link>
+          <Link to="/prestamos" className="text-gray-500 hover:text-gray-900">Prestamos</Link>
+        </nav>
+    );
+}
