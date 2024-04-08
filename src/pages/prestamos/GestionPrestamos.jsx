f967854ac61d710d5a44878d1c58649e6c1fde4b
jjeg1979 <jjeg1979@gmail.com>
2024-04-08 14:48:48 +0200

    Actualizar página GestionPrestamos

--- src/pages/prestamos/GestionPrestamos.jsx
+++ src/pages/prestamos/GestionPrestamos.jsx
@@ -1,8 +1,36 @@
-export default function GestionPretamos() {
-    return (
-        <div>
-            <h1>Gestión de Préstamos</h1>
-        </div>
-    )
+import { useAccount, useContractRead } from 'wagmi'
+import { ErrorInfo, SocioPrincipal, TotalLoanBalance } from '../../components/ui'
+import { PrestamoDefiABI } from '../../contracts/ABIs'
+import DepositarGarantia from '../usuarios/DepositarGarantia';
 
-}
+
+export default function GestionPretamos() {
+
+    const { address, isConnected } = useAccount()
+
+    const { data } = useContractRead({
+    address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
+    abi: PrestamoDefiABI,
+    functionName: 'socioPrincipal',
+    }); 
+
+    return (
+      <div className="flex flex-col self-center sm:grid place-items-center px-3 py-16 md:px-5 gap-12">
+        <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl mb-2 bg-gradient-to-r from-indigo-600 to-green-400 text-transparent bg-clip-text">
+        Blockmaker PrestamosDeFi DApp
+      </h1>
+
+      {isConnected && (address === data) ? (
+        <>            
+            <TotalLoanBalance />
+            <SocioPrincipal />
+            <DepositarGarantia />
+        </>
+      ) : (
+        <>
+            <ErrorInfo message="Por favor, autentíquese como Socio Principal para poder gestionar los usuarios" />   
+        </>
+      )}
+      </div>
+    )
+  }
