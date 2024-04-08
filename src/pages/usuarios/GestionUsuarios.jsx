bc936d9be27417bf4a7f41cb23d4670b969f836b
jjeg1979 <jjeg1979@gmail.com>
2024-04-08 12:56:40 +0200

    Añadir comprobación usuarios a GestiónUsuarios

--- src/pages/usuarios/GestionUsuarios.jsx
+++ src/pages/usuarios/GestionUsuarios.jsx
@@ -1,20 +1,28 @@
-import { useAccount } from 'wagmi'
-import { SocioPrincipal, TotalLoanBalance } from '../../components/ui'
+import { useAccount, useContractRead } from 'wagmi'
+import { ErrorInfo, SocioPrincipal, TotalLoanBalance } from '../../components/ui'
 import { AltaPrestamista, AltaCliente}  from '../usuarios'
+import { PrestamoDefiABI } from '../../contracts/ABIs'
+
 
 export default function GestionUsuarios() {
 
-    const { isConnected } = useAccount()
+    const { address, isConnected } = useAccount()
 
-   
+    const { data } = useContractRead({
+    address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
+    abi: PrestamoDefiABI,
+    functionName: 'socioPrincipal',
+    }); 
+
     return (
       <div className="flex flex-col self-center sm:grid place-items-center px-3 py-16 md:px-5 gap-12">
         <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl mb-2 bg-gradient-to-r from-indigo-600 to-green-400 text-transparent bg-clip-text">
         Blockmaker PrestamosDeFi DApp
       </h1>
 
-      {isConnected ? (
+      {isConnected && (address === data) ? (
         <>
+            
           <TotalLoanBalance />
           <SocioPrincipal />
           <AltaPrestamista />
@@ -22,7 +30,7 @@ export default function GestionUsuarios() {
         </>
       ) : (
         <>
-          
+            <ErrorInfo message="Por favor, autentíquese como Socio Principal para poder gestionar los usuarios" />   
         </>
       )}
       </div>
