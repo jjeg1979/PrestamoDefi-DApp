435c3db8515a9a54320fa75cf2d44b72eb31ba4d
jjeg1979 <jjeg1979@gmail.com>
2024-04-08 10:21:46 +0200

    Añadir sólo mostrar Usuarios y Prestamos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { ConnectKitButton } from 'connectkit'
 import { Navbar } from '../components'
+import { useAccount } from 'wagmi'
 
 export default function Header() {
+
+    const { isConnected } = useAccount()
+
     return (
       <header className="py-2 px-3 sm:py-4 sm:px-8 flex justify-between items-center bg-white border-b shadow-xs ">
         {/* Logo para version mobile */}
@@ -9,7 +13,8 @@ export default function Header() {
         {/* Logo para version desktop */}
         <img src="../blockmaker-full-logo.png" alt="blockmaker-desktop-logo" width={300} className="hidden sm:flex" />        
         {/* Botton de conexion wallet con prop showBalance para mostrar balance cuenta */}
-        <Navbar />
+        { isConnected ? (<Navbar />) : (null) }
+        
         <ConnectKitButton showBalance />        
       </header>
     )
