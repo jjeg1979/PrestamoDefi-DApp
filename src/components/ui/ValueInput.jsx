7c43858c9c1440f2a9fe5361571304bf82ba6be5
jjeg1979 <jjeg1979@gmail.com>
2024-04-09 11:57:19 +0200

    Corregido ValueInput

--- src/components/ui/ValueInput.jsx
+++ src/components/ui/ValueInput.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
-export default function ValueInput({ type = 'text', placeholder, value, disabled}) {
+export default function ValueInput({ type = 'text', placeholder, value, onChange, disabled}) {
     return (
         <div>
             <input type={type}
-            value={value} 
+            value={value}
+            onChange={onChange}
             placeholder={placeholder}
             disabled={disabled}
             className="border border-gray-300 rounded-lg p-2 outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-300 ease-in-out disabled:cursor-not-allowed"
@@ -15,6 +16,7 @@ export default function ValueInput({ type = 'text', placeholder, value, disabled
 ValueInput.propTypes = {
     type: PropTypes.string,
     placeholder: PropTypes.string,
+    onChange: PropTypes.func,
     value: PropTypes.number,
     disabled: PropTypes.bool,
 }
