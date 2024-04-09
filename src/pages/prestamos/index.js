6b8a162292f66a4d6a17dd7d3a4e41e65ff264f5
jjeg1979 <jjeg1979@gmail.com>
2024-04-09 13:26:43 +0200

    Añadir componente ReembolsarPrestamo al index.js

--- src/pages/prestamos/index.js
+++ src/pages/prestamos/index.js
@@ -1,3 +1,4 @@
 export { default as GestionPrestamos } from "./GestionPrestamos";
 export { default as SolicitarPrestamo } from "./SolicitarPrestamo";
 export { default as AprobarPrestamo } from "./AprobarPrestamo";
+export { default as ReembolsarPrestamo } from "./ReembolsarPrestamo";
