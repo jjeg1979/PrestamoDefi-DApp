aeb493c7ea8463bdf95f00bb9ec18ad6767d34b8
jjeg1979 <jjeg1979@gmail.com>
2024-04-09 13:15:51 +0200

    Añadir Componente AprobarPrestamo al index.js

--- src/pages/prestamos/index.js
+++ src/pages/prestamos/index.js
@@ -1,2 +1,3 @@
 export { default as GestionPrestamos } from "./GestionPrestamos";
 export { default as SolicitarPrestamo } from "./SolicitarPrestamo";
+export { default as AprobarPrestamo } from "./AprobarPrestamo";
