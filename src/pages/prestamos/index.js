b33d888efdd7e7db29a6105f83e8a6f78398c0b6
jjeg1979 <jjeg1979@gmail.com>
2024-04-09 13:35:52 +0200

    Añadir compoenente LiquidarGarantia a index.js

--- src/pages/prestamos/index.js
+++ src/pages/prestamos/index.js
@@ -2,3 +2,4 @@ export { default as GestionPrestamos } from "./GestionPrestamos";
 export { default as SolicitarPrestamo } from "./SolicitarPrestamo";
 export { default as AprobarPrestamo } from "./AprobarPrestamo";
 export { default as ReembolsarPrestamo } from "./ReembolsarPrestamo";
+export { default as LiquidarGarantia } from "./LiquidarGarantia";
