1c3f495f36ecc33ceeb1fc5fc3e0762d17063bb7
jjeg1979 <jjeg1979@gmail.com>
2024-04-10 07:39:51 +0200

    Añadir Componente PrestamosClientes

--- src/pages/prestamos/PrestamosCliente.jsx
+++ src/pages/prestamos/PrestamosCliente.jsx
@@ -1,66 +1,88 @@
-import { Card, Typography } from "@materia-tailwind/react";
-import { useContractRead, useAccount } from "wagmi";
+import { useContractReads, useContractRead, useAccount } from "wagmi";
 import { PrestamoDefiABI } from "../../contracts/ABIs";
+import { Title } from "../../components/ui";
+import Web3 from 'web3';
 
-const TABLE_HEAD = ["Monto", "Plazo", "Estado"];
+const prestamoDeFiContract = {
+  address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
+  abi: PrestamoDefiABI,
+};
+
 
 export default function PrestamosCliente() {
 
     const { address } = useAccount()
 
-    const { data, isError, isLoading } = useContractRead({
-    address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
-    abi: PrestamoDefiABI,
-    functionName: 'obtenerPrestamosPorPrestatario',
-    args: [address]
+    // Lectura de los ids de los préstamos del cliente
+
+    const { data: idsPrestamosCliente } = useContractRead({
+      ...prestamoDeFiContract,
+      functionName: 'obtenerPrestamosPorPrestatario',
+      args: [address]
     });
 
+    // Construcción de las llamadas a useContractReads para obtener los detalles de los préstamos
+    const consultasPrestamos = idsPrestamosCliente.map((id) => ({
+      ...prestamoDeFiContract,
+      functionName: 'obtenerDetallesDePrestamo',
+      args: [address, id]
+    }))    
+
+    const { data: detallesPrestamosCliente } = useContractReads({
+      contracts: consultasPrestamos
+    });  
 
     return (
-      <Card className="h-full w-full overflow-scroll">
-        <table className="w-full min-w-max table-auto text-left">
-          <thead>
-            <tr>
-              {TABLE_HEAD.map((head) => (
-                <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal leading-none opacity-70"
-                  >
-                    {head}
-                  </Typography>
-                </th>
-              ))}
-            </tr>
+      <section className="bg-white p-4 border shadow w-fit rounded-lg text-sm space-y-2" >
+        <Title>Préstamos Cliente</Title>
+
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-400 uppercase bg-indigo-800 text-center">
+              <tr>
+                  <th scope="col" className="px-6 py-3">
+                      Préstamo id
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                      Monto (ETH)
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                      Plazo (días)
+                  </th>
+                  <th scope="col" className="px-6 py-3 text">
+                      Aprobado
+                  </th>
+                  <th scope="col" className="px-6 py-3 text">
+                      Reembolsado
+                  </th>
+              </tr>
           </thead>
           <tbody>
-            {TABLE_ROWS.map(({ monto, plazo, estado }, index) => (
-              <tr key={name} className="even:bg-blue-gray-50/50">
-                <td className="p-4">
-                  <Typography variant="small" color="blue-gray" className="font-normal">
-                    {name}
-                  </Typography>
-                </td>
-                <td className="p-4">
-                  <Typography variant="small" color="blue-gray" className="font-normal">
-                    {job}
-                  </Typography>
-                </td>
-                <td className="p-4">
-                  <Typography variant="small" color="blue-gray" className="font-normal">
-                    {date}
-                  </Typography>
-                </td>
-                <td className="p-4">
-                  <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
-                    Edit
-                  </Typography>
-                </td>
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  
+                  {detallesPrestamosCliente.map((prestamo, index) => (
+                    <>
+                      <th key={`${index.toString()+1}`} scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
+                        {(prestamo.result.id).toString()}
+                      </th>                      
+                      <td key={`${index.toString()+2}`} className="px-6 py-4 text-center">
+                        {Web3.utils.fromWei(prestamo.result?.monto, 'ether')}
+                      </td>
+                      <td key={`${index.toString()+3}`} className="px-6 py-4 text-center">
+                        {(prestamo.result?.plazo/BigInt('1036400')).toString()}
+                      </td>
+                      <td key={`${index.toString()+4}`} className="px-6 py-4 text-center">
+                        {prestamo.result?.aprobado ? 'SÍ' : 'NO'}
+                      </td>
+                      <td key={`${index.toString()+5}`} className="px-6 py-4">
+                        {prestamo.result?.reembolsado ? 'SÍ' : 'NO'}
+                      </td>
+                    </>               
+                    ))}
+                 
               </tr>
-            ))}
           </tbody>
-        </table>
-      </Card>
+    </table>
+        
+      </section>
     );
   }
